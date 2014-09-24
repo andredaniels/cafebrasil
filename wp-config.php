a01659761825e1a5013d4ce1741fd458e77b259c
Andre Daniels <andre777@ucsc.edu>
2014-09-24 11:11:16 -0700

    test

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'trickdog_wor5184');
+define('DB_NAME', 'xxxxxx');
 
 /** MySQL database username */
 define('DB_USER', 'xxxxx');
